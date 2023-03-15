ccb0ba66f038c81120b88784e9e135ef25b28748
Hema <96591537+HemaSahuRathore@users.noreply.github.com>
2023-03-15 16:35:22 +0530

    Update dockerrun.sh

--- scripts/dockerrun.sh
+++ scripts/dockerrun.sh
@@ -1,2 +1,3 @@
 sudo docker pull hemasahur/samplejavaapp:latest
-sudo docker run -itd -p 8003:8080 hemasahur/samplejavaapp:latest
+sudo docker rm -f javaapplication
+sudo docker run --name javaapplication -itd -p 8003:8080 hemasahur/samplejavaapp:latest
