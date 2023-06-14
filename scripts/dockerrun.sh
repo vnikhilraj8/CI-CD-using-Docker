765a068d7772176931b10978b104af4c469a7d4a
vnikhilraj8 <103923293+vnikhilraj8@users.noreply.github.com>
2023-06-14 15:21:17 +0530

    Update dockerrun.sh

--- scripts/dockerrun.sh
+++ scripts/dockerrun.sh
@@ -1,3 +1,3 @@
 sudo docker pull nikhilrajvakapalli/bridgelabs-test:javaappimg
 sudo docker rm -f javaapplication
-sudo docker run --name javaapplication -itd -p 8003:8080 nikhilrajvakapalli/bridgelabs-test:javaappimg
+sudo docker run --name javaapplication -itd -p 8080:8080 nikhilrajvakapalli/bridgelabs-test:javaappimg
